ae10b0f35157f60f9ff33b9acc74b77eef179e2f
DanTinho <161615116+DanTinho@users.noreply.github.com>
2025-07-29 22:46:16 -0300

    Add files via upload

--- lojapy/app.py
+++ lojapy/app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 # Dados de exemplo para os produtos
 produtos = [
-    {"id": 1, "nome": "Vara de Pesca com Molinete 200m", "preco": 249.90, "imagem": "vara_de_pesca.jpg"},
-    {"id": 2, "nome": "Linha de Pesca Dourado 100g", "preco": 59.90, "imagem": "linha.jpg"},
+    {"id": 1, "nome": "Vara de Pesca com Molinete 200m", "preco": 149.90, "imagem": "vara_de_pesca.jpg"},
+    {"id": 2, "nome": "Linha de Pesca Dourado 100g 600 Metros", "preco": 49.90, "imagem": "linha.jpg"},
     {"id": 3, "nome": "Jogo de Anzol Black Chinu 10 unidades", "preco": 19.90, "imagem": "anzol.jpg"},
     {"id": 4, "nome": "Isca Artificial 12cm", "preco": 14.90, "imagem": "isca.jpg"},
 ]
@@ -17,6 +17,15 @@ def index():
 @app.route('/produtos')
 def mostrar_produtos():
     return render_template('produtos.html', produtos=produtos)
+
+@app.route('/produto/<int:produto_id>')
+def produto_unico(produto_id):
+    produto = next((p for p in produtos if p["id"] == produto_id), None)
+
+    if produto is None:
+        ConnectionAbortedError
+    else:
+        return render_template('produto_específico.html', produto=produto)
 
 @app.route('/sobre')
 def sobre():
